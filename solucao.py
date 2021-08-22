e67807280d1ed2678acc4067d81b21e4ab24d630
tminuzzo <tminuzzo@inf.ufrgs.br>
2021-08-22 18:54:29 -0300

    Change order to display public methods/attributes first. Change double to single underscore on private.

--- solucao.py
+++ solucao.py
@@ -40,29 +40,55 @@ class Nodo:
     @custo.setter
     def custo(self, value):
         self.__custo = value
-
-    def _custo_prioridades(self):
-        return self._h() + self._g()
     
-    def _h(self):
-        return distancia_hamming(self.estado)
-
-    def _g(self):
-        return self.custo
-
     def __str__(self):
         return "(%s, %s, %s, %d)" % (self.estado, self.pai.estado, self.acao, self.custo)
     
     def __lt__(self, other):
         return self._custo_prioridades() < other._custo_prioridades()
 
+    def _custo_prioridades(self):
+        return self._h() + self._g()   
+    
+    def _h(self):
+        return self._distancia_hamming(self.estado)
 
-def __string_para_array(estado: str) -> np.ndarray:
-    return np.array(list(estado), dtype=str).reshape((3, 3))
+    def _g(self):
+        return self.custo
+    
+    def _distancia_hamming(self, inicial, objetivo="12345678_" ):
+        """
+        computa o número de peças fora do lugar, dado um estado inicial e final
 
+        :param inicial: str
+        :param objetivo: str, optional
+        :return:
+        """
 
-def __array_para_string(estado: np.ndarray) -> str:
-    return "".join(estado.ravel())
+        count = 0
+        for index, value in enumerate(objetivo):
+            if inicial[index] != objetivo[index]:
+                count += 1
+
+        return count
+
+class FilaPrioridade:
+    """
+    classe que encapsula métodos do módulo python heapq - Heap queue algorithm: 
+    https://docs.python.org/3/library/heapq.html
+
+    """
+    def __init__(self):
+        self.fila = []
+
+    def push(self, item):
+        heappush(self.fila, item)
+    
+    def pop(self):
+        return heappop(self.fila)
+    
+    def __len__(self):
+        return len(self.fila)
 
 
 def sucessor(estado: str) -> List[Tuple[str, str]]:
@@ -77,7 +103,7 @@ def sucessor(estado: str) -> List[Tuple[str, str]]:
 
     estados = []
 
-    estado = __string_para_array(estado)
+    estado = _string_para_array(estado)
 
     posicao_espaco = np.where(estado == "_")
     i, j = posicao_espaco[0].item(), posicao_espaco[1].item()
@@ -87,28 +113,28 @@ def sucessor(estado: str) -> List[Tuple[str, str]]:
         estado_acima[i][j] = estado_acima[i - 1][j]
         estado_acima[i - 1][j] = "_"
 
-        estados.append(("acima", __array_para_string(estado_acima)))
+        estados.append(("acima", _array_para_string(estado_acima)))
 
     if i < 2:  # i.e. Se não está na linha mais abaixo
         estado_abaixo = estado.copy()
         estado_abaixo[i][j] = estado_abaixo[i + 1][j]
         estado_abaixo[i + 1][j] = "_"
 
-        estados.append(("abaixo", __array_para_string(estado_abaixo)))
+        estados.append(("abaixo", _array_para_string(estado_abaixo)))
 
     if j < 2:  # i.e. Se não está na coluna mais a direita
         estado_direita = estado.copy()
         estado_direita[i][j] = estado_direita[i][j + 1]
         estado_direita[i][j + 1] = "_"
 
-        estados.append(("direita", __array_para_string(estado_direita)))
+        estados.append(("direita", _array_para_string(estado_direita)))
 
     if j > 0:  # i.e. # i.e. Se não está na coluna mais a esquerda
         estado_esquerda = estado.copy()
         estado_esquerda[i][j] = estado_esquerda[i][j - 1]
         estado_esquerda[i][j - 1] = "_"
 
-        estados.append(("esquerda", __array_para_string(estado_esquerda)))
+        estados.append(("esquerda", _array_para_string(estado_esquerda)))
 
     return estados
 
@@ -130,22 +156,72 @@ def expande(nodo: Nodo) -> List[Nodo]:
     return filhos
 
 
-def __obter_caminho(nodo: Nodo) -> List[str]:
-    """
-    Dado um nodo, retorna o conjunto de ações que levam a esse nodo.
 
-    :param nodo: objeto da classe Nodo
+def bfs(estado: str) -> List[str]:
+    """
+    Recebe um estado (string), executa a busca em LARGURA e
+    retorna uma lista de ações que leva do estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None.
+
+    :param estado: str
     :return:
     """
-    caminho = []
 
-    while nodo.pai is not None:
-        caminho.append(nodo.acao)
-        nodo = nodo.pai
+    def retira(fronteira: Deque[Nodo]) -> Tuple[Nodo, Deque[Nodo]]:
+        v = fronteira.popleft()
 
-    caminho.reverse()
+        return v, fronteira
 
-    return caminho
+    return _busca_grafo(estado, retira)
+
+
+def dfs(estado: str) -> List[str]:
+    """
+    Recebe um estado (string), executa a busca em PROFUNDIDADE e
+    retorna uma lista de ações que leva do estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None.
+
+    :param estado: str
+    :return:
+    """
+
+    def retira(fronteira: Deque[Nodo]) -> Tuple[Nodo, Deque[Nodo]]:
+        v = fronteira.pop()
+
+        return v, fronteira
+
+    return _busca_grafo(estado, retira)
+
+
+def astar_hamming(estado: str) -> List[str]:
+    """
+    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Hamming e
+    retorna uma lista de ações que leva do estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None.
+
+    :param estado: str
+    :return:
+    """
+
+    def retira(fronteira): # TODO: Usar typehinting na classe customizada FilaPrioridade
+        v = fronteira.pop()
+
+        return v, fronteira
+
+    return _busca_grafo_prioridades(estado, retira)
+
+
+def astar_manhattan(estado: str) -> List[str]:
+    """
+    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Manhattan e
+    retorna uma lista de ações que leva do estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None.
+
+    :param estado: str
+    :return:
+    """
+    # substituir a linha abaixo pelo seu codigo
+    raise NotImplementedError
 
 
 def executa_caminho(estado_inicial: str, caminho: List[str]) -> str or None:
@@ -174,105 +250,9 @@ def executa_caminho(estado_inicial: str, caminho: List[str]) -> str or None:
                 break
 
     return nodo.estado
+    
 
-
-def __busca_grafo(estado_incial: str, retira: Callable[[Deque[Nodo]], Tuple[Nodo, Deque[Nodo]]],
-                  estado_objetivo="12345678_") -> List[str] or None:
-    """
-    Busca num grafo o estado_final partindo do estado_inicial, usando a função retira para remover
-    itens da fronteira. Retorna o caminho do estado_inicial até o estado_final se este existir;
-    None caso o contrário.
-
-    :param estado_incial: str
-    :param retira: callable, função que remove um item da fronteira
-    :param estado_objetivo: str, optional
-    :return:
-    """
-
-    explorados = set()  # Ordem não importa
-    fronteira = deque()
-    fronteira.append(Nodo(estado_incial, None, None, 0))
-
-    while len(fronteira) > 0:
-        v, fronteira = retira(fronteira)
-
-        if v.estado == estado_objetivo:
-            return __obter_caminho(v)
-
-        if v.estado not in explorados:
-            explorados.add(v.estado)
-            fronteira.extend(expande(v))
-
-    return None
-
-
-def bfs(estado: str) -> List[str]:
-    """
-    Recebe um estado (string), executa a busca em LARGURA e
-    retorna uma lista de ações que leva do estado recebido até o objetivo ("12345678_").
-    Caso não haja solução a partir do estado recebido, retorna None.
-
-    :param estado: str
-    :return:
-    """
-
-    def retira(fronteira: Deque[Nodo]) -> Tuple[Nodo, Deque[Nodo]]:
-        v = fronteira.popleft()
-
-        return v, fronteira
-
-    return __busca_grafo(estado, retira)
-
-
-def dfs(estado: str) -> List[str]:
-    """
-    Recebe um estado (string), executa a busca em PROFUNDIDADE e
-    retorna uma lista de ações que leva do estado recebido até o objetivo ("12345678_").
-    Caso não haja solução a partir do estado recebido, retorna None.
-
-    :param estado: str
-    :return:
-    """
-
-    def retira(fronteira: Deque[Nodo]) -> Tuple[Nodo, Deque[Nodo]]:
-        v = fronteira.pop()
-
-        return v, fronteira
-
-    return __busca_grafo(estado, retira)
-
-
-def astar_hamming(estado: str) -> List[str]:
-    """
-    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Hamming e
-    retorna uma lista de ações que leva do estado recebido até o objetivo ("12345678_").
-    Caso não haja solução a partir do estado recebido, retorna None.
-
-    :param estado: str
-    :return:
-    """
-
-    def retira(fronteira): # TODO: Usar typehinting na classe customizada FilaPrioridade
-        v = fronteira.pop()
-
-        return v, fronteira
-
-    return __busca_grafo_prioridades(estado, retira)
-
-
-def astar_manhattan(estado: str) -> List[str]:
-    """
-    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Manhattan e
-    retorna uma lista de ações que leva do estado recebido até o objetivo ("12345678_").
-    Caso não haja solução a partir do estado recebido, retorna None.
-
-    :param estado: str
-    :return:
-    """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
-
-def __busca_grafo_prioridades(estado_incial: str, retira,
+def _busca_grafo_prioridades(estado_incial: str, retira,
                   estado_objetivo="12345678_") -> List[str] or None:
     """
     Busca num grafo o estado_final partindo do estado_inicial, usando a função retira para remover
@@ -293,7 +273,7 @@ def __busca_grafo_prioridades(estado_incial: str, retira,
         v, fronteira = retira(fronteira)
 
         if v.estado == estado_objetivo:
-            return __obter_caminho(v)
+            return _obter_caminho(v)
 
         if v.estado not in explorados:
             explorados.add(v.estado)
@@ -304,36 +284,57 @@ def __busca_grafo_prioridades(estado_incial: str, retira,
 
     return None
 
-def distancia_hamming(inicial, objetivo="12345678_" ):
-    """
-    computa o número de peças fora do lugar, dado um estado inicial e final
 
-    :param inicial: str
-    :param objetivo: str, optional
+def _busca_grafo(estado_incial: str, retira: Callable[[Deque[Nodo]], Tuple[Nodo, Deque[Nodo]]],
+                  estado_objetivo="12345678_") -> List[str] or None:
+    """
+    Busca num grafo o estado_final partindo do estado_inicial, usando a função retira para remover
+    itens da fronteira. Retorna o caminho do estado_inicial até o estado_final se este existir;
+    None caso o contrário.
+
+    :param estado_incial: str
+    :param retira: callable, função que remove um item da fronteira
+    :param estado_objetivo: str, optional
     :return:
     """
 
-    count = 0
-    for index, value in enumerate(objetivo):
-        if inicial[index] != objetivo[index]:
-            count += 1
+    explorados = set()  # Ordem não importa
+    fronteira = deque()
+    fronteira.append(Nodo(estado_incial, None, None, 0))
 
-    return count
+    while len(fronteira) > 0:
+        v, fronteira = retira(fronteira)
 
-class FilaPrioridade:
+        if v.estado == estado_objetivo:
+            return _obter_caminho(v)
+
+        if v.estado not in explorados:
+            explorados.add(v.estado)
+            fronteira.extend(expande(v))
+
+    return None
+
+def _string_para_array(estado: str) -> np.ndarray:
+    return np.array(list(estado), dtype=str).reshape((3, 3))
+
+
+def _array_para_string(estado: np.ndarray) -> str:
+    return "".join(estado.ravel())
+
+
+def _obter_caminho(nodo: Nodo) -> List[str]:
     """
-    classe que encapsula métodos do módulo python heapq - Heap queue algorithm: 
-    https://docs.python.org/3/library/heapq.html
+    Dado um nodo, retorna o conjunto de ações que levam a esse nodo.
 
+    :param nodo: objeto da classe Nodo
+    :return:
     """
-    def __init__(self):
-        self.fila = []
+    caminho = []
 
-    def push(self, item):
-        heappush(self.fila, item)
-    
-    def pop(self):
-        return heappop(self.fila)
-    
-    def __len__(self):
-        return len(self.fila)
+    while nodo.pai is not None:
+        caminho.append(nodo.acao)
+        nodo = nodo.pai
+
+    caminho.reverse()
+
+    return caminho
